a5b7a54a97a572abe25b7a6ac6a57e574ec9524c
Vlad Morozov <morozov90vlad@mail.ru>
2024-02-24 15:27:26 +0300

    HW3 tasks 1,2,3 done. Models successfully migrated.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    preview = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория')
+    price = models.IntegerField(null=True, blank=True, verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(null=True, blank=True, auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
